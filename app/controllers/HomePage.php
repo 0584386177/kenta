327ee564c6ed9cd3f78d0785eb2ce9ed08d5068e
Hoa <huynhkhanhhoa3006@gmail.com>
2025-02-26 01:50:51 +0700

    Upload

---
+++ app/controllers/HomePage.php
@@ -0,0 +1,9 @@
+<?php
+
+class HomePage{
+    
+    public function index(){
+        echo "HOMEPAGE_INDEX";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once './app/server.php';
+
+$myApp = new App;
+?>
